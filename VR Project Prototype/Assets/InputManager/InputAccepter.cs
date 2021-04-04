e5d5ddb3dbe82a6babff6a6d4a1c550aa61f42a8
beep-boop-coder <68105181+beep-boop-coder@users.noreply.github.com>
2021-04-04 22:15:02 +0800

    initial prototype

---
+++ VR Project Prototype/Assets/InputManager/InputAccepter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputAccepter : MonoBehaviour
+{
+    private void Awake(){
+        PlayerInput.onTriggerDown += TriggerDown;
+        PlayerInput.onTriggerUp += TriggerUp;
+    }
+    private void TriggerDown(){
+        print("Trigger down");
+    }
+
+    private void TriggerUp(){
+        print("Trigger up");
+    }
+}
---
+++ VR Project Prototype/Assets/InputManager/PlayerInput.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInput : MonoBehaviour
+{
+    public static UnityAction<bool> onHasController = null;
+
+    private bool hasController = false;
+    private bool inputActive = true;
+
+    public static UnityAction onTriggerUp = null;
+    public static UnityAction onTriggerDown = null;
+    public static UnityAction onTouchpadUp = null;
+    public static UnityAction onTouchpadDown = null;
+
+    private void Awake(){
+        OVRManager.HMDMounted += PlayerFound;
+        OVRManager.HMDUnmounted += PlayerLost;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnDestroy(){
+        OVRManager.HMDMounted -= PlayerFound;
+        OVRManager.HMDUnmounted -= PlayerLost;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inputActive)
+            return;
+
+            hasController = CheckForController(hasController);
+
+        //This event will fire once the trigger is pushed down
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger)){
+            if(onTriggerDown != null){
+                onTriggerDown();
+            }
+        }
+        //This event will fire once the trigger is not pushed down
+        if(OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger)){
+            if(onTriggerUp != null){
+                onTriggerUp();
+            }
+        }
+        //This event will fire once the touchpad is pushed down
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad)){
+            if(onTriggerDown != null){
+                onTouchpadDown();
+            }
+        }
+        //This event will fire once the touchpad is not pushed down
+        if(OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad)){
+            if(onTriggerUp != null){
+                onTriggerUp();
+            }
+        }
+
+        float triggerValue = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+
+        if(triggerValue > 0.5f){
+            //do stuff in here
+        }
+        
+    }
+
+    private bool CheckForController(bool currentValue){
+        bool controllerCheck = OVRInput.IsControllerConnected(OVRInput.Controller.RTrackedRemote);
+
+        if(currentValue == controllerCheck)
+            return currentValue;
+
+        if(onHasController != null)
+            onHasController(controllerCheck);
+
+
+        return controllerCheck;
+    }
+
+    private void PlayerFound(){
+        inputActive = true;
+    }
+
+    private void PlayerLost(){
+        inputActive = false;
+    }
+}
