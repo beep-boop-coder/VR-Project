bbb24768bceff38547eb938ff8fb6a3a78f493b5
beep-boop-coder <68105181+beep-boop-coder@users.noreply.github.com>
2021-04-10 19:29:28 +0800

    Added second level

---
+++ VR Project Prototype/Assets/Scripts/ComputerInput.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This script take in computer input to simulate VR control
+
+public class ComputerInput : MonoBehaviour
+{
+    public float speed = 3;
+
+
+    void Update()
+    {
+        // This would rotate the camera by pressing and dragging to simulate VR rotation
+        if (Input.GetMouseButton(0))
+        {
+            transform.RotateAround(transform.position, -Vector3.up, speed * Input.GetAxis("Mouse X"));
+            transform.RotateAround(transform.position, transform.right, speed * Input.GetAxis("Mouse Y"));
+
+        }
+
+        // Simulate trigger pressed
+        if (Input.GetKey(KeyCode.T))
+        { 
+            SceneManager.LoadScene("360VideoTest");
+        }
+    }
+}
---
+++ VR Project Prototype/Assets/Scripts/VRPlayerInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VRPlayerInput : MonoBehaviour
+{
+
+
+    void Update()
+    { 
+
+        // Trigger pressed
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            SceneManager.LoadScene("360VideoTest");
+        }
+    }
+}
